dabe50bb85a02289b6cf43782d02bfbd82d1e5c0
Jaky ChanART <jakydadandmom@gmail.com>
2019-10-17 16:50:09 -0500

    Juego7

---
+++ TDA/DadoCrack.py
@@ -0,0 +1,13 @@
+import random
+
+class Dado:
+    def _init_(self):
+        self.dado = 0
+
+    def tirarDado(random):
+        self.dado = random.randint(1, 6)
+
+    def getDado(self):
+        return self.dado
+    
+        
---
+++ TDA/Jugador.py
@@ -0,0 +1,26 @@
+
+
+class Jugador:
+    def _init__(self, capital):
+        self.capital = capital
+        self.apuesta = 0
+        self.jugada = 0
+
+        def setApuesta(self, apuesta):
+            self.apuesta = apuesta
+
+        def setJugada(self, jugada):
+            self.jugada = jugada
+
+        def setCapital(self, capital):
+            self.capital = capital
+
+        def getCapital(self):
+            return set.capital
+
+        def getApuesta(self):
+            return set.apuesta
+
+        def getJugada(self):
+            return set.jugada
+            
